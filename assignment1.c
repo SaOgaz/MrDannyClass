ccf1c6a57aefd5f281b8de6de44ba84c2c104eda
SaraOgaz <ogaz@stsci.edu>
2015-08-05 17:33:21 -0400

    standard check in

---
+++ assignment1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+
+
+
+/* dont' forget to return 0 */
+
+
+/* pass by value, but not arrays (obv) */
+
+/* null characer '\0' */
--- fahrenheit.c
+++ fahrenheit.c
@@ -4,6 +4,8 @@
 #define UPPER 300   /* upper limit */
 #define STEP  20    /* step size */
 
+double convert(int temp);
+
 /* print Fahrenheit-Celsius table
    for fahr = 0, 20, ...., 300 */
 
@@ -14,6 +16,13 @@ main()
   printf("fahr     cel\n");
 
   for (fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP){
-    printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
-  }     
+    printf("%3d %6.1f\n", fahr, convert(fahr));
+  }  
+  return 0;   
+}
+
+
+/*convert fahr to celsius*/
+double convert(int temp){
+  return ((5.0/9.0)*(temp-32));
 }
